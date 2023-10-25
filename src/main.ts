6c817c9145b5dbdd3632c64f9aca0e0e60ba9963
Andrea Lopez <andrealoga33@gmail.com>
2023-10-25 21:08:50 +0200

    Create components for the exercise

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
-document.querySelector<HTMLDivElement>('.app')!.innerHTML = `
-<h1>Holi</h1>
-`;
+/* eslint-disable no-new */
+// document.querySelector<HTMLDivElement>('.app')!.innerHTML = `
+// <h1>Holi</h1>
+// `;
+
+import { List } from './components/list';
+import './scss/style.scss';
+
+function main() {
+  new List('.app');
+}
+
+main();
